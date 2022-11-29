53d204883fdacc22c45134b68bfa1ea43a6c5b4a
alessiam1909 <ale.menon.vr@gmail.com>
2022-11-29 15:18:39 +0100

    inserisco nel ciclo le condizioni, se multiplo di 3 stampo Fizz, se multiplo di 5 stampo Buzz, se multiplo di entrambi stampo FizzBuzz, altrimenti stampo solo il numero

--- Js/script.js
+++ Js/script.js
@@ -3,5 +3,19 @@
 let num = 1;
 
 for (let i = 1; i <= 100; i ++ ){
-    console.log(i);
-}
+    
+//     inserisco le condizioni:
+//    2.1 Se il numero è multiplo di 3 stampa “Fizz”
+//    2.2 Se il numero è multiplo di 5 stampa "Buzz"
+//    2.3 Se il numero è multiplo di entrambi stampa "“FizzBuzz”
+//    2.4 Altrimenti stampa il numero
+    if (i % 3 == 0 && i % 5 == 0){
+        console.log("FizzBuzz");
+    } else if(i % 3 == 0){
+        console.log("Fizz");
+    } else if(i % 5 == 0){
+        console.log("Buzz");
+    } else {
+        console.log(i);
+    }
+};
