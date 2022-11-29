0098db59682cb6fcf9b82a2d7393a6f4227a7466
alessiam1909 <ale.menon.vr@gmail.com>
2022-11-29 16:04:51 +0100

    stilizzazione e inserimento degli elementi nella pagina

--- Js/script.js
+++ Js/script.js
@@ -1,6 +1,8 @@
 // Creo un ciclo che stampi i numeri da 1 a 100
-
+const container = document.querySelector(".container");
 let num = 1;
+
+let box; 
 
 for (let i = 1; i <= 100; i ++ ){
     
@@ -10,12 +12,22 @@ for (let i = 1; i <= 100; i ++ ){
 //    2.3 Se il numero è multiplo di entrambi stampa "“FizzBuzz”
 //    2.4 Altrimenti stampa il numero
     if (i % 3 == 0 && i % 5 == 0){
+
+        box = `<div class="box box-pink">${`FizzBuzz`}</div>`;
         console.log("FizzBuzz");
     } else if(i % 3 == 0){
+
+        box = `<div class="box box-acqua">${`Fizz`}</div>`;
         console.log("Fizz");
     } else if(i % 5 == 0){
+
+        box = `<div class="box box-yellow">${`Buzz`}</div>`;
         console.log("Buzz");
     } else {
+
+        box = `<div class="box box-lb">${num}</div>`;
         console.log(i);
     }
+    num++;
+    container.innerHTML += box;
 };
